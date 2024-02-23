37f08820945085bbc40f329de7d5c68cf8d96227
Nicolas Castro <nik_ced@hotmail.com>
2024-02-23 13:00:53 -0500

    useNavigate

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { blogdata } from '../data/blogdata';
 
 export const BlogPost = () => {
-  const { slug } = useParams();
-  const blogpost = blogdata.find(post => post.slug === slug);
+    const { slug } = useParams();
+    const blogpost = blogdata.find(post => post.slug === slug);
+    
+    const navigate = useNavigate();
 
-  if (!blogpost) {
-    // Manejar el caso en que no se encuentre la publicación
-    return <p>Blog post no encontrado.</p>;
-  }
+    const returnToBlog = () => {
+        navigate(-1)
+    }
 
-  return (
-    <div className="container mx-auto p-4">
-    <article className=" bg-stone-600 p-6 rounded-md shadow-md">
-      <h2 className="text-3xl font-bold mb-4 text-white">{blogpost.title}</h2>
-      <p className="text-yellow-300 mb-2">Por {blogpost.author}</p>
-      <p className='text-gray-200'>{blogpost.content}</p>
-    </article>
-  </div>
-  );
+    if (!blogpost) {
+        // Manejar el caso en que no se encuentre la publicación
+        return <p>Blog post no encontrado.</p>;
+    }
+
+    return (
+        <div className="container mx-auto p-4">
+            <button
+                onClick={returnToBlog}>Volver al blog</button>
+            <article className=" bg-stone-600 p-6 rounded-md shadow-md">
+                <h2 className="text-3xl font-bold mb-4 text-white">{blogpost.title}</h2>
+                <p className="text-yellow-300 mb-2">Por {blogpost.author}</p>
+                <p className='text-gray-200'>{blogpost.content}</p>
+            </article>
+        </div>
+    );
 };
